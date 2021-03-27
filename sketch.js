6442a50e257a5cd6fc7d9c5b8ed9c142d91bd8b5
ojas-vinze <71540421+ojas-vinze@users.noreply.github.com>
2021-03-27 09:40:23 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,19 @@
-var dog,happydog,dogimg,database,foods,foodstock,foodcount,foodobj,lastfed,fedtime;
+var dog,happydog,dogimg,database,foods,foodstock,foodcount,foodobj,lastfed,fedtime,milk,milkimg;
 var feedb,addfoodb;
 
 function preload(){
   happydog=loadImage("dogimg2.png");
-  dogimg=loadImage("dogimg1.png")
+  dogimg=loadImage("dogimg1.png");
+  milkimg=loadImage("Milk.png");
 }
 
 function setup(){
   createCanvas(1200, 570);
   
   foodobj = new Foodc();
-  foodobj.foodstock=20;
+  // foodobj.foodstock=20;
+  foodobj.getfeedtime();
+  foodobj.getfoodstock();
   
   
   dog = createSprite(600,285,30,70);
@@ -19,12 +22,12 @@ function setup(){
 
   database=firebase.database();
 
-  foodstock=database.ref('Food');
-  foodstock.on("value",readstock);
-  fedtime=database.ref('Feedtime');
-  fedtime.on("value",function(data){
-    lastfed=data.val();
-  })
+  // foodstock=database.ref('Food');
+  // foodstock.on("value",readstock);
+  // fedtime=database.ref('Feedtime');
+  // fedtime.on("value",function(data){
+  //   lastfed=data.val();
+  // })
 
   feedb = createButton('Feed the dog');
   feedb.position(1000,30);
@@ -34,6 +37,11 @@ function setup(){
   addfoodb = createButton('Add food');
   addfoodb.position(1100,30);
   addfoodb.mousePressed(addfood);
+
+  milk = createSprite(480,350,80,80);
+  milk.addImage(milkimg);
+  milk.scale=0.15;
+  milk.visible=false;
 }
 
 function draw(){
@@ -50,26 +58,30 @@ function draw(){
   }
 
   foodobj.diaplay();
-  console.log(foodobj.foodstock);
+  // console.log(foodobj.foodstock);
 
   drawSprites();
 }
 
 function readstock(data){
   foods=data.val();
+  console.log(foods);
 }
 
 function feeddog(){
+  milk.visible=true;
   dog.addImage(happydog);
-  foodobj.updatefoodstock(foodobj.getfoodstock()-1);
+  // foodobj.updatefoodstock(foodobj.getfoodstock()-1);
   database.ref('/').update({
-    Food:foodobj.getfoodstock(),
+    Food:foods-1,
     Feedtime:hour()
   })
 }
 
 function addfood(){
+  console.log("Hi, in here!");
   foods++;
+  console.log("add "+foods);
   database.ref('/').update({
     Food:foods
   })
